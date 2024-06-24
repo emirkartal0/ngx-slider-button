23441ecc0835ec82d69b55f95b4683e202b84193
emir <emirkartal00@gmail.com>
2024-06-24 12:50:36 +0300

    initial commit

---
+++ projects/ngx-slider-button/src/lib/ngx-slider-button.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-slider-button',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ngx-slider-button works!
+    </p>
+  `,
+  styles: ``
+})
+export class NgxSliderButtonComponent {
+
+}
